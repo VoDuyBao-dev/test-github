8d63bf1bd04c2b5a90f30f214d1a60fc589059f9
VoDuyBao-dev <voduybao19052005@gmail.com>
2024-12-07 17:01:10 +0700

    day len lan 5

--- tesst2.cpp
+++ tesst2.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int main() {
     cout << "Test github lan 2 ne!" << endl;
     cout << "Tets lan 2 ne 7/12" << endl;
-<<<<<<< HEAD
     cout << "test 4h50" << endl;
-=======
-    cout << "bao bao" << endl;
->>>>>>> b2ff9662f55fb8f833ad7d0f414e6bf2c23f505e
+    cout << "test nx ne";
     return 0;
 }
